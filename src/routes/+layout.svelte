b8d99b98bd97362d7d489ddfcc2b01054187ec0b
illusice_isc <sheryl.fire.1995@gmail.com>
2025-10-07 22:51:55 +0900

    初回コミット

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
+	import favicon from '$lib/assets/favicon.svg';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<title>ネームプレート作成ツール</title>
+	<link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children?.()}
